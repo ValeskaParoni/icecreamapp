b9614da2e7ff826b11d33c1e7ed238d90df8417c
ValeskaParoni <valeskaparoni@gmail.com>
2017-07-10 16:50:31 -0300

    basic login

---
+++ src/error.py
@@ -0,0 +1,9 @@
+USERNAME = False
+PASSWORD = False
+
+
+def clear():
+    USERNAME = False
+    PASSWORD = False
+    print(USERNAME)
+    print(PASSWORD)
---
+++ src/routes.py
@@ -0,0 +1,67 @@
+from flask import Flask, session, request, render_template, redirect
+import auth
+
+app = Flask(__name__)
+app.secret_key = '34whiufgiug8342'
+
+
+
+@app.route('/login', methods=["GET"])
+def login_page():
+    if 'logged_in' in session:
+        return redirect('/home')
+    return render_template('login_page.txt', login_post_url = '/login', create_account_url = '/newaccount', username_error = auth.errors['username'], password_error = auth.errors['password'])
+
+@app.route('/login', methods=["POST"])
+def check_login():
+    if (auth.check_user(request.form['username'], request.form['pass'])):
+        session['logged_in'] = True
+        session['name'] = auth.get_name(request.form['username'])
+        return redirect('/home')
+    return redirect('/login')
+
+
+
+
+
+@app.route('/newaccount', methods=["GET"])
+def render_new_account_page():
+    if 'logged_in' in session:
+        return redirect('/home')
+    return render_template('new_account_form.txt', create_account_post_url = '/newaccount', username_error = auth.errors['username'])
+
+@app.route('/newaccount', methods=["POST"])
+def create_new_account():
+    if auth.new_user(request.form['username'], request.form['pass'], request.form['name']):
+        return redirect('/login', code = 307)
+    return redirect('/newaccount')
+
+
+
+
+
+@app.route('/logout')
+def logout():
+    if 'logged_in' in session:
+        session.pop('logged_in')
+    if 'name' in session:
+        session.pop('name')
+    return redirect('/login')
+
+
+
+
+
+@app.route('/home')
+def home_page():
+    if 'logged_in' in session:
+        return render_template('home_page.txt', name = session['name'], logout_url = '/logout')
+    return redirect('/login')
+
+@app.route('/')
+def default():
+    return redirect('/home')
+
+
+if __name__ == '__main__':
+    app.run()
